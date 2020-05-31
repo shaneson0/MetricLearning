1fddfa0339f8ec34c366f2ac5d4f7b20228ef234
shanxuan <781244184@qq.com>
2020-05-31 19:39:03 +0800

    add Domain

--- utils/settings.py
+++ utils/settings.py
@@ -3,11 +3,11 @@ import os
 
 Domain = "Aminer"
 
-PROJ_DIR = join(abspath(dirname(__file__)), '..', Domain)
+PROJ_DIR = join(abspath(dirname(__file__)), '..')
 
-DATA_DIR = join(PROJ_DIR, 'data')
-OUT_DIR = join(PROJ_DIR, 'out')
-PIC_DIR = join(PROJ_DIR, 'pic')
+DATA_DIR = join(PROJ_DIR, 'data', Domain)
+OUT_DIR = join(PROJ_DIR, 'out', Domain)
+PIC_DIR = join(PROJ_DIR, 'pic', Domain)
 EMB_DATA_DIR = join(DATA_DIR, 'emb')
 GLOBAL_DATA_DIR = join(DATA_DIR, 'global')
 os.makedirs(OUT_DIR, exist_ok=True)
